195047f8f4e998a46de75f2e765a6944eb8aa02f
pol-93 <polcerdan@gmail.com>
2020-04-13 14:35:33 +0200

    Crete User
    Password Encripted

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,22 +8,15 @@ use App\Form\CreateUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-<<<<<<< HEAD
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-=======
->>>>>>> 1c0bc86e7b61873b7ce3d6ed84fbb2604d0d0c3b
 
 class RegisterController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      */
-<<<<<<< HEAD
     public function index(Request $request, UserPasswordEncoderInterface $encoder)
-=======
-    public function index(Request $request)
->>>>>>> 1c0bc86e7b61873b7ce3d6ed84fbb2604d0d0c3b
-    { 
+    {
 
         $user = new User();
         $form = $this->createForm(CreateUserType::class, $user);
@@ -32,11 +25,8 @@ class RegisterController extends AbstractController
         if($form->isSubmitted() && $form->isValid()){
             $em = $this->getDoctrine()->getManager();
             $user->setRoles(0);
-<<<<<<< HEAD
             $encoded = $encoder->encodePassword($user, $form['password']->getData());
             $user->setPassword($encoded);
-=======
->>>>>>> 1c0bc86e7b61873b7ce3d6ed84fbb2604d0d0c3b
             $em->persist($user);
             $em->flush();
             $this->addFlash('success', 'User Created!');
