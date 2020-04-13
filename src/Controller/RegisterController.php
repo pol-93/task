bb81d0c0238d6f01884e9fc4bb3ef88fb862a3cf
pol-93 <polcerdan@gmail.com>
2020-04-13 14:33:14 +0200

    Crete User
    Password Encripted

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\CreateUserType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $encoder)
+    { 
+
+        $user = new User();
+        $form = $this->createForm(CreateUserType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $user->setRoles(0);
+            $encoded = $encoder->encodePassword($user, $form['password']->getData());
+            $user->setPassword($encoded);
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'User Created!');
+            return $this->redirectToRoute('register');
+        }
+
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
+            'createUser' => $form->createView()
+        ]);
+    }
+}
